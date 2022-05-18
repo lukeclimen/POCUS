321095978c1cae4f432a401150e70f9777bdc55e
Luke Climenhage <luke.climenhage@gmail.com>
2022-05-18 13:38:54 -0400

    Added videos, modals, clickable image, updated script

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 $(function() {
-    let openModalButtons = $('[data-modal-target]');
+    let openModalButtons = $('[data-target]');
     let closeModalButtons = $('[data-close-button]');
     let overlay = $('.overlay');
+
     openModalButtons
     .click(function() {
-        $('.modal').addClass("modal-active");
+        let modal = $(this).attr("data-target");
+        console.log(modal);
+        $(modal).addClass("modal-active");
         $('.overlay').addClass("overlay-active");
     });
+
     closeModalButtons
     .click(function() {
         $('.modal').removeClass("modal-active");
